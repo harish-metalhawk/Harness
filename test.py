ba3a0fd967adf8e578891af9eb0cb0a8d2d41663
harish reddy <harish.b310@gmail.com>
2011-10-19 15:42:07 +0530

    fixed a small typo

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 #!/home/buildmaster/sw/python/2.6.5/linux/x86_64/bin/python2.6
-from changet_harness2 import implemtation
+from changed_harness2 import implemtation
 from adv_auto_start import check_for_new_build,get_latest_build
 
 def parser():
