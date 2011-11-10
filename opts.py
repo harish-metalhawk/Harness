05e16871c4a34cd7c3eaf4d9a7ebe1daa551f5db
harish reddy <harish.b310@gmail.com>
2011-11-10 19:36:59 +0530

    made simple harness the default in the opts.py

--- opts.py
+++ opts.py
@@ -7,7 +7,7 @@ def opts():
     parse.add_option('-m',dest='conf',type='string',help="This is a compulsary option which provides the path to master conf")
     parse.add_option('-c',dest='current',action='store_true',default=False,help="This is options makes the jobs run in the currently available latest builds")
     parse.add_option("-p",dest='path',type='string',default="/home/buildmaster/nightly/AVM/main-dev-x86/in_progress/latest",help='this option sets the path of the branch to look out for new build , by default its the main-dev build')
-    parse.add_option('-n',dest='plain',action='store_true',default=False,help="this mode makes the application run as a simple harness without any build-detection")
+    parse.add_option('-n',dest='plain',action='store_true',default=True,help="this mode makes the application run as a simple harness without any build-detection")
     if parse.parse_args()[0].plain and ( parse.parse_args()[0].build or parse.parse_args()[0].path != "/home/buildmaster/nightly/AVM/main-dev-x86/in_progress/latest") :
         parse.error("incompatible options")
     if parse.parse_args()[0].conf is None :
